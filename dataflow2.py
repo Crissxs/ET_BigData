a4d2993e63511d2dd7085786f4024d8dec0263d2
Crissxs <cri.carrasco@duocuc.cl>
2024-05-19 18:01:09 -0400

    a

--- dataflow2.py
+++ dataflow2.py
@@ -1,13 +1,20 @@
 import apache_beam as beam
-from apache_beam.options.pipeline_options import PipelineOptions
+from apache_beam.options.pipeline_options import PipelineOptions, StandardOptions, SetupOptions
 import logging
+import argparse
 
 def run(argv=None):
     """Main entry point; defines and runs the pipeline."""
     
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--output', dest='output', required=True, help='Output file to write results to.')
+    known_args, pipeline_args = parser.parse_known_args(argv)
+
     # Definir las opciones de la pipeline
-    pipeline_options = PipelineOptions(argv)
-    
+    pipeline_options = PipelineOptions(pipeline_args)
+    pipeline_options.view_as(StandardOptions).runner = 'DataflowRunner'
+    pipeline_options.view_as(SetupOptions).save_main_session = True
+
     # Crear el pipeline
     with beam.Pipeline(options=pipeline_options) as p:
         
@@ -18,7 +25,7 @@ def run(argv=None):
         )
         
         # Escribir la salida a un archivo de texto
-        output | 'Write' >> beam.io.WriteToText('output/hola.txt')
+        output | 'Write' >> beam.io.WriteToText(known_args.output)
     
 if __name__ == '__main__':
     logging.getLogger().setLevel(logging.INFO)
