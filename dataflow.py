e307a0865a6fc15a28a930214484b722c556b827
Crissxs <cri.carrasco@duocuc.cl>
2024-05-18 21:42:44 -0400

    dataflow
    Para descargar mas rapido a GCS

---
+++ dataflow.py
@@ -0,0 +1,68 @@
+import json
+import requests
+import os
+import apache_beam as beam
+from apache_beam.options.pipeline_options import PipelineOptions
+from apache_beam.io.gcp.gcsio import GcsIO
+from google.cloud import storage
+import zipfile
+import tempfile
+
+class DownloadAndExtractFiles(beam.DoFn):
+    def process(self, element):
+        url = element['url']
+        response = requests.get(url)
+        filename = url.split('/')[-1]
+        folder_name = filename.split('.')[0]
+
+        # Save to a temporary file
+        with tempfile.NamedTemporaryFile(delete=False) as temp_file:
+            temp_file.write(response.content)
+            temp_filename = temp_file.name
+
+        # Extract the zip file
+        with zipfile.ZipFile(temp_filename, 'r') as zip_ref:
+            zip_ref.extractall('/tmp/' + folder_name)
+
+        # Upload extracted files to GCS
+        storage_client = storage.Client()
+        bucket_name = 'duoc-red-bucket'
+        bucket = storage_client.bucket(bucket_name)
+        
+        for root, _, files in os.walk('/tmp/' + folder_name):
+            for file in files:
+                local_file_path = os.path.join(root, file)
+                gcs_path = os.path.join('Datos_Historicos', folder_name, file)
+                blob = bucket.blob(gcs_path)
+                blob.upload_from_filename(local_file_path)
+                yield f'Uploaded {gcs_path}'
+
+        # Clean up temporary files
+        os.remove(temp_filename)
+        for root, _, files in os.walk('/tmp/' + folder_name):
+            for file in files:
+                os.remove(os.path.join(root, file))
+
+def run(argv=None):
+    pipeline_options = PipelineOptions(argv)
+    p = beam.Pipeline(options=pipeline_options)
+
+    input_json = 'gs://duoc-red-bucket/data.json'
+    
+    # Download the JSON file from GCS
+    storage_client = storage.Client()
+    bucket = storage_client.bucket('duoc-red-bucket')
+    blob = bucket.blob('data.json')
+    data = json.loads(blob.download_as_text())
+
+    resources = data['result']['resources']
+    
+    (p
+     | 'Read URLs' >> beam.Create(resources)
+     | 'Download and Extract Files' >> beam.ParDo(DownloadAndExtractFiles())
+     | 'Write Results' >> beam.io.WriteToText('gs://duoc-red-bucket/results/output'))
+
+    p.run().wait_until_finish()
+
+if __name__ == '__main__':
+    run()
